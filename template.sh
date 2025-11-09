3cf04d93603142ed1482f6acee5c13bb78cd06b4
kalesha001 <kalesha@kalesha>
2025-11-09 19:55:38 +0530

    folder sturcture created

---
+++ template.sh
@@ -0,0 +1,18 @@
+#Creating directory
+mkdir -p src
+mkdir -p research
+
+#Creating files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully"
+
