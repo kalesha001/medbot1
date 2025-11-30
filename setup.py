79984ddd27e1971f1541f8a355f51fb387536c43
Kalesha001 <kaleshavali.shaik0001@gmail.com>
2025-11-30 19:09:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup
+setup(
+    name="medbot1",
+    version="0.1.0",
+    author="kalesha001",
+    author_email="kaleshavali.shaik001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)  
